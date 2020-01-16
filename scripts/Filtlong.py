d0ea4469490692f93092ebffdcac1ee6b8d6cc70
AStieva <55895868+AStieva@users.noreply.github.com>
2020-01-16 12:56:53 +0100

    Temp + prefix

--- scripts/Filtlong.py
+++ scripts/Filtlong.py
@@ -12,6 +12,6 @@ def filtlong(tool, inp, len):
     print("Running Filtlong")
 
     # The min length could be changed, or is 1000. The min mean q stands for the minimum quality, the 1 percentile worst quality reads are cut out.
-    os.system("{} --min_length {} --min_mean_q 1 {} > {}".format(tool, len, inp, os.path.join(args.outdir, "temp_filtlong.fastq")))
+    os.system("{} --min_length {} --min_mean_q 1 {} > {}".format(tool, len, inp, os.path.join(args.outdir, "temp_{}_filtlong.fastq".format(prefix))))
 
     print("Filtlong finished")
